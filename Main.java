72a92bd2590a07e4ecf3131e8daee13748680c56
andreecalderon <159475179+andreecalderon@users.noreply.github.com>
2024-07-19 12:43:48 -0600

    panel

--- Main.java
+++ Main.java
@@ -9,6 +9,10 @@ import conversor.grados;
 import Lista.ListaDeCompras;
 import conversor.conversorMoneda;
 import java.util.Scanner;
+import java.awt.*;
+import java.util.Scanner;
+
+import javax.swing.*;
 
 public class Main {
 
@@ -22,6 +26,52 @@ public class Main {
                 "conversor de moneda", "Biblioteca",
                 "Colegio", "Salir"
         };
+           //sección grafica
+        JFrame miVentana = new JFrame();
+        miVentana.setBounds(0, 0, 500, 600  );
+        miVentana.setLayout(null);
+
+
+            // PANEL DEL BANNER
+            JPanel banner = new JPanel();
+            banner.setBackground(Color.BLUE);
+
+            miVentana.add(banner);
+            banner.setBounds(0, 0, 500, 300 );
+
+            miVentana.setVisible(true);
+
+        JFrame frame = new JFrame("Rockema");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new BorderLayout());
+
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.add(mainPanel);
+
+
+        JPanel textPanel = new JPanel(new GridLayout(3, 2, 5, 5)); // 3 filas, 2 columnas, con espacio entre componentes
+        textPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Márgenes
+
+
+        for (int i = 1; i <= 6; i++) {
+            JLabel label = new JLabel("G3", SwingConstants.CENTER);
+            label.setFont(new Font("Arial", Font.PLAIN, 14));
+            textPanel.add(label);
+        }
+        mainPanel.add(textPanel, BorderLayout.CENTER);
+
+
+        JButton button = new JButton("save");
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 200, 10, 10)); // Márgenes
+        buttonPanel.add(button);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+
+        frame.setVisible(true);
 
         boolean progra = true;
         do {
